7dc66295b315e0d61b15d0e07e4068ed0ae9bfff
Cessalya <91775097+Cessalya@users.noreply.github.com>
2022-02-25 08:19:26 +0700

    Update siswa.controller.js

--- contollers/siswa.controller.js
+++ contollers/siswa.controller.js
@@ -1,9 +1,17 @@
 'use strict'
-
+const bcrypt = require('bcrypt')
 const req = require('express/lib/request')
 const res = require('express/lib/response')
+const jwt = require('jsonwebtoken')
 const db = require('../db')
 
+const secret = '#$@^%$^%*&%$$@&'
+
+
+function hashPassword(password){
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
+}
 module.exports = {
     read: (req, res) =>{
         const sql = "select * from siswa"
@@ -64,5 +72,39 @@ module.exports = {
                 message: "Berhasil menghapus data"
             })
         })
-    }
+    }, 
+    registrasi: (req, res) => {
+        const{
+            nama,
+            email,
+            password
+        }= req.body
+        if (!nama, !email || !password) res.status(402).json({message: 'nama, email dan password harus diisi'})
+        return db.query('insert into akun_siswa set ?', {nama, email, password: hashPassword(password)}, (err, result) =>{
+            if(err) return res.status(500).json({err})
+
+            return res.json({message: 'registrasi berhasil', data: result})
+        })   
+    },
+    login: (req, res) => {
+        const {
+            email, 
+            password
+        } = req.body
+        
+        if( !email || !password) res.status(402).json({message: "email dan password harus diisi."})
+
+        return db.query('select * from akun where email = ?', email , (err, result)=>{
+            if(err) return res.status(500).json({err})
+
+            const user = result[0]
+            if(typeof user === 'undefined')return res.status(401).json({message: "user tidak ditemukan"})
+            if(!bcrypt.compareSync(password, user.password)) return res.status(401).json({message: "email atau password tidak sesuai"})
+
+            const token = jwt.sign({data: user}, secret)
+
+            return res.json({message: 'login berhasil. silahkan menggunakan token dibawah ini untuk mengakses endpoint private lain', token})
+        })
+
+    },
 }
